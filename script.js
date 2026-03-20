db8590c925918e64814c5812dd8da385fba80dde
suhailaf <suhailaf004@gmail.com>
2026-03-20 17:58:23 +0800

    Initial Little Dino website

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+  navLinks.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("show");
+    });
+  });
+}
